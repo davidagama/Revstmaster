bedef5d6bac3e7c1f4664e5e80d53306ecfc44ac
Davi <mondromodg@hotmail.com>
2016-07-09 09:09:13 -0300

    atualização de arquivos
    asta atualizado

---
+++ Arquivos/BD/RevstMaster.sql
@@ -0,0 +1,15 @@
+﻿create database RevstMaster;
+
+create table calculo
+(id_calculo serial primary key, 
+nome varchar(50), 
+tipo varchar(30), 
+altura numeric(10,3), 
+largura numeric(10,3), 
+comprimento numeric(10,3),
+valor numeric(10,3), id_relatorio );
+
+create table relatório
+(id_relatorio serial primary key, 
+nome varchar (30),  
+
